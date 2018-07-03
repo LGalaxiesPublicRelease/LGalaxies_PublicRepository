98a35f00309b976774f7c7201f26229f4316cc9e
Peter Thomas <p.a.thomas@sussex.ac.uk>
2018-07-03 09:05:52 +0100

    h_metals.h: Code to make a union for metals so that it can be accessed as either a structure or an array Subdirectory to hold all the run scripts.

--- code/h_metals.h
+++ code/h_metals.h
@@ -10,8 +10,43 @@ struct metals
 //Number of chemical elements tracked:
 #ifndef MAINELEMENTS
 #define NUM_ELEMENTS 11 //All: [H][He][C][N][O][Ne][Mg][Si][S][Ca][Fe]
+struct elements_str
+{
+    float H;
+    float He;
+    float C;
+    float N;
+    float O;
+    float Ne;
+    float Mg;
+    float Si;
+    float S;
+    float Ca;
+    float Fe;
+};
 #else
 #define NUM_ELEMENTS 5 //Only [H][He][O][Mg][Fe]
-#endif
+struct elements_str
+{
+    float H;
+    float He;
+    float O;
+    float Mg;
+    float Fe;
+};
+#endif  //MAINELEMENTS
+/* Define two views into the same element structure/array.
+   For example define:
+      union elements DiskMassElements;
+   Then access as either
+      DiskMassElements.str.H
+   or
+      DiskMassElements.arr[0]
+*/
+union elements
+{
+    struct elements_str str;
+    float arr[NUM_ELEMENTS];
+};
 #endif //INDIVIDUAL_ELEMENTS
 #endif //DETAILED_METALS_AND_MASS_RETURN
